f5fd7192af7b3bfd891527fd760a5b533e71c2a1
kaoliiny <katja0407@ukr.net>
2022-01-31 00:24:57 +0200

    Add main logic for the tasks

--- main/display_data.c
+++ main/display_data.c
@@ -1,12 +1,36 @@
 
 #include <stdio.h>
+#include <time.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "dataQueue.h"
+#include "esp_log.h"
+#include "data_queue.h"
 
-void displayQueueData(void *cb) {
+/* 1s = 1000ms */
+#define DELAY_MS    1000
+
+void print_queue_data(message_t *message) {
+	if (message == NULL) {
+		return ;
+	}
+
+	printf("counter value: %d\n", message->counter);
+	printf("timestamp: %s\n", ctime((const long int *)&message->time_stamp));
+}
+
+void displayQueueData(void *data) {
+	message_t *msg_queue = NULL;
+
+	/* Nothig to receiv yet */
+	vTaskDelay(DELAY_MS * 5 / portTICK_PERIOD_MS);
+
 	while (1) {
-		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		if (xQueueReceive(counter_queue_handle, (void *)&msg_queue, (TickType_t)0) != pdPASS) {
+			ESP_LOGW(QUEUE_TAG, "Queue receive error!");
+		} else {
+			print_queue_data(msg_queue);
+		}
+		vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
 	}
 }
--- main/put_data.c
+++ main/put_data.c
@@ -1,15 +1,32 @@
 
 #include <stdio.h>
+#include <time.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "dataQueue.h"
+#include "esp_log.h"
+#include "data_queue.h"
 
 /* 5s = 5000ms */
 #define DELAY_MS    5000
 
-void dataQueueUpdate(void *cb) {
+static message_t msg_queue = {0};
+
+void dataQueueUpdate(void *data) {
+	message_t *msg = &msg_queue;
+
 	while (1) {
+		if (uxQueueSpacesAvailable(counter_queue_handle) > 0) {
+			/* Don't update counter and time_stamp when the queue is already full. */
+			msg->counter++;
+			msg->time_stamp = time(NULL);
+
+			/* Don't block if the queue is already full. */
+			if (xQueueSend(counter_queue_handle, (void *)&msg, (TickType_t)0) != pdPASS) {
+				ESP_LOGW(QUEUE_TAG, "Queue send error!");
+			}
+		}
+		/* (￣o￣) zzZzzZ */
 		vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
 	}
 }
